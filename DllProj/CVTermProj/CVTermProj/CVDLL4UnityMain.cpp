61115b2a26da1d226a81d777fd09f4175c60cf56
SungPhill Moon <sp9103@naver.com>
2015-03-02 16:37:33 +0900

    -Kinect 연동 완료

--- DllProj/CVTermProj/CVTermProj/CVDLL4UnityMain.cpp
+++ DllProj/CVTermProj/CVTermProj/CVDLL4UnityMain.cpp
@@ -1,9 +1,53 @@
-#include "define.h"
+#include <Windows.h>
+
+#include "KinectConnector.h"
 
 #define EXPORT_API __declspec(dllexport)
 
 extern "C"{
+	KinectConnector kinect;
+	HRESULT hr;
+
+	//Image allocation
+	Mat KinectRGB, KinectDepth;
+
+	SkeletonInfo tBodystruct;
+
 	void EXPORT_API TEST(){
 		MessageBox(NULL, L"TEST SUCCESS", L"TEST", MB_OK);
+	}
+
+	void EXPORT_API KinectInitialize(){
+		KinectRGB.create(KINECT_COLOR_HEIGHT, KINECT_COLOR_WIDTH, CV_8UC4);
+		KinectDepth.create(KINECT_DEPTH_HEIGHT, KINECT_DEPTH_WIDTH, CV_8UC4);
+
+		cv::namedWindow("RGB", CV_WINDOW_KEEPRATIO);
+		cv::namedWindow("DEPTH", CV_WINDOW_KEEPRATIO);
+
+		kinect.KinectInitialize(KinectSource_Color | KinectSource_Depth| KinectSource_Body | KinectSource_Face);
+	}
+
+	void EXPORT_API KinectDeinitialize(){
+		kinect.KinectDestroy();
+
+		//Image release
+		KinectRGB.release();
+		KinectDepth.release();
+
+		cv::destroyAllWindows();
+	}
+
+	int EXPORT_API KinectRun(){
+		kinect.GetDepthImage(&KinectDepth);
+		hr = kinect.GetColorImage(&KinectRGB);
+		if(SUCCEEDED(hr))
+			hr = kinect.GetSkeletonPos(&tBodystruct, &KinectRGB, 0);
+		else
+			return -1;
+
+		imshow("RGB", KinectRGB);
+		imshow("DEPTH", KinectDepth);
+
+		return 1;
 	}
 }
