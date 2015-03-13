30b485aec29984636593d029e4b545e6d02dcdb4
SungPhill Moon <sp9103@naver.com>
2015-03-13 17:21:09 +0900

    Revert "Revert "-Kinect 연동 완료""
    This reverts commit 5a63e9c68ce764d02e8ce63a1804a274d8ce96d6.

---
+++ UnityProj/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[DllImport("CVTermProj")]
+	public static extern void TEST();
+	[DllImport("CVTermProj")]
+	public static extern void KinectInitialize();
+	[DllImport("CVTermProj")]
+	public static extern void KinectDeinitialize();
+	[DllImport("CVTermProj")]
+	public static extern int KinectRun();
+	
+	// Use this for initialization
+	void Start () {
+		TEST ();
+		KinectInitialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		KinectRun ();
+	}
+
+	void OnDestroy(){
+		Debug.Log ("Deinitialize");
+		KinectDeinitialize ();
+	}
+}
